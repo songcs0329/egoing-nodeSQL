9f867ba4bc058a88bd9f558f4839932ba906ab43
songcs <songcs0329@gmail.com>
2021-09-12 16:37:10 +0900

    author 테이블 쿼리 모듈 추가

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const url = require('url');
 const topic = require('./lib/topic');
+const author = require('./lib/author');
 
 const app = http.createServer((request, response) => {
 	const _url = request.url;
@@ -16,6 +17,12 @@ const app = http.createServer((request, response) => {
 	else if(pathname === '/update') topic.update(request, response, queryData.id)
 	else if(pathname === '/update_process') topic.updateProcess(request, response)
 	else if(pathname === '/delete_process') topic.deleteProcess(request, response)
+	else if(pathname === '/author') author.home(request, response)
+	else if(pathname === '/author/create') author.create(request, response)
+	else if(pathname === '/author/create_author') author.createAuthor(request, response)
+	else if(pathname === '/author/update') author.update(request, response, queryData.id)
+	else if(pathname === '/author/update_author') author.updateAuthor(request, response)
+	else if(pathname === '/author/delete_author') author.deleteAuthor(request, response)
 	else {
 		response.writeHead(404);
 		response.end('Not found');
